96a120d8609b3e572b4a17bc2de60c96975b7bad
YuanJun222 <117643016+YuanJun222@users.noreply.github.com>
2022-11-07 23:31:42 +0900

    model.py

--- hw3/model.py
+++ hw3/model.py
@@ -96,11 +96,11 @@ class Transcriber_RNN(nn.Module):
         # Notice: Changing the initialization order may fail the tests.
         self.melspectrogram = LogMelSpectrogram()
 
-        self.frame_lstm = torch.nn.LSTM(n_mels, 88, num_layers=2)
-        self.frame_fc = torch.nn.Linear(n_mels,88)
+        self.frame_lstm = torch.nn.LSTM(N_MELS, 88, num_layers=2)
+        self.frame_fc = torch.nn.Linear(N_MELS,88)
 
-        self.onset_lstm = torch.nn.LSTM(n_mels, 88, num_layers=2)
-        self.onset_fc = torch.nn.Linear(n_mels,88)
+        self.onset_lstm = torch.nn.LSTM(N_MELS, 88, num_layers=2)
+        self.onset_fc = torch.nn.Linear(N_MELS,88)
 
     def forward(self, audio):
         # TODO: Question 1
